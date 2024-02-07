4239faf09e5973062eb262517ed1c1526f0dcd57
Clifton <117292357+Clifton-TS@users.noreply.github.com>
2024-02-07 10:23:51 -0300

    Add draw handling and start layout

--- script.js
+++ script.js
@@ -19,10 +19,13 @@ function createBoard () {
             [0, 0, 0],
             [0, 0, 0]
         ],
+
+        timesPlayed: 0,
         
         insertSymbol(player, x, y) {
             if (this.board[x][y] == 0) {
                 this.board[x][y] = player.symbol
+                this.timesPlayed++
             }
         },
 
@@ -37,25 +40,27 @@ function createBoard () {
                 || this.board[0][0] + this.board[1][1] + this.board[2][2] == trips
                 || this.board[0][2] + this.board[1][1] + this.board[2][0] == trips){
                     return player.symbol
+                }else if (this.timesPlayed >= 9) {
+                    return "draw"
                 }
         }
+
     }
 }
 
-botao = document.getElementById("botao")
 playerX = createPlayer("birunda", "X", 0)
 playerO = createPlayer("peixola", "O", 0)
 
 board = createBoard()
-board.insertSymbol(playerO, 0, 1)
-board.insertSymbol(playerO, 1, 1)
+board.insertSymbol(playerX, 0, 1)
+board.insertSymbol(playerX, 1, 1)
 board.insertSymbol(playerO, 2, 1)
+board.insertSymbol(playerO, 2, 0)
+// board.insertSymbol(playerX, 2, 2)
+board.insertSymbol(playerO, 0, 0)
+board.insertSymbol(playerO, 0, 2)
+board.insertSymbol(playerX, 1, 0)
+board.insertSymbol(playerO, 1, 2)
 console.log(board.board)
 console.log(board.board[0][1] + board.board[1][1] + board.board[2][1])
 console.log(board.checkForWin(playerO))
-
-botao.addEventListener("click", () => {
-    player1.addScore()
-    console.log(player1.getFullName())
-})
-
